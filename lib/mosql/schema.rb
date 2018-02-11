e2ac7137f7c887bb88daa0e9ae71fd256c42c116
Eric Coutu <eric.coutu@gmail.com>
2018-02-11 17:16:11 -0500

    Add option to drop table with cascade

--- lib/mosql/schema.rb
+++ lib/mosql/schema.rb
@@ -89,6 +89,9 @@ module MoSQL
           meta = collection[:meta]
           composite_key = meta[:composite_key]
           keys = []
+          if clobber and meta[:force_drop]
+            db.drop_table(meta[:table], cascade: true)
+          end
           db.send(clobber ? :create_table! : :create_table?, meta[:table]) do
             collection[:columns].each do |col|
               opts = {}
